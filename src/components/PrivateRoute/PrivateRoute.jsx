7f36ebed48b0098e66a41fcfb0f53e2d9cd5f1a8
Andriy Zhylchuk <zhylchuk.andriy@gmail.com>
2024-10-16 16:51:17 +0300

    add component PrivateRoute

---
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+/**
+ * - Якщо користувач увійшов у систему, відобразити компонент
+ * - В іншому випадку відобразити <Navigate> для перенаправлення
+ */
+
+// import { useSelector } from "react-redux";
+// import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import { Navigate } from "react-router-dom";
+
+export default function PrivateRoute({ component, redirectTo }) {
+  // Поки немає логіки логіну, вручну змінюйте на true/false
+  const isLoggedIn = false;
+
+  return isLoggedIn ? component : <Navigate to={redirectTo} />;
+}
--- src/components/RestrictedRoute/RestrictedRoute.jsx
+++ src/components/RestrictedRoute/RestrictedRoute.jsx
@@ -3,12 +3,13 @@
  * - В іншому випадку відобразити компонент
  */
 
-import { useSelector } from "react-redux";
-import { selectIsLoggedIn } from "../../redux/auth/selectors";
+// import { useSelector } from "react-redux";
+// import { selectIsLoggedIn } from "../../redux/auth/selectors";
 import { Navigate } from "react-router-dom";
 
 export default function RestrictedRoute({ component, redirectTo }) {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
+  // Поки немає логіки логіну, вручну змінюйте на true/false
+  const isLoggedIn = false;
 
   return isLoggedIn ? <Navigate to={redirectTo} /> : component;
 }
