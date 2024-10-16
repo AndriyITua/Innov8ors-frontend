931e4298ff24c417e7abecaa87d0d6521ad10cdd
Andriy Zhylchuk <zhylchuk.andriy@gmail.com>
2024-10-16 15:34:41 +0300

    add new components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,1 +1,50 @@
-export default function App() {}
+import { lazy, Suspense, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+
+const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+const SigninPage = lazy(() => import("../../pages/SigninPage/SigninPage"));
+const SignupPage = lazy(() => import("../../pages/SignupPage/SignupPage"));
+const WelcomePage = lazy(() => import("../../pages/WelcomePage/WelcomePage"));
+
+export default function App() {
+  return (
+    <>
+      {/* <Header /> */}
+      <Suspense fallback={<p></p>}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <RestrictedRoute component={<WelcomePage />} redirectTo="/home" />
+            }
+          />
+          <Route
+            path="/welcome"
+            element={
+              <RestrictedRoute component={<WelcomePage />} redirectTo="/home" />
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <RestrictedRoute component={<SignupPage />} redirectTo="/home" />
+            }
+          />
+          <Route
+            path="/signin"
+            element={
+              <RestrictedRoute component={<SigninPage />} redirectTo="/home" />
+            }
+          />
+          <Route
+            path="/home"
+            element={
+              <PrivateRoute component={<HomePage />} redirectTo="/signin" />
+            }
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
+}
