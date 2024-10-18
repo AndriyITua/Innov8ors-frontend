54fdcc1c626c8528d6c931cf321f4388a1601626
kholshevnikova <151967304+kholshevnikova@users.noreply.github.com>
2024-10-18 17:55:56 +0300

    fix background/form styles

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -5,11 +5,18 @@ import css from "./SignupPage.module.css";
 export default function SignupPage() {
   return (
     <>
-      <p>Signup Page</p>
       <section className={css.signupSection}>
         <div className={css.container}>
           <h3 className={css.formText}>Sign Up</h3>
           <AuthForm />
+        </div>
+        <div className={css.bottleImage}>
+          <img
+            src="../../assets/signupPageImages/bottle-signup.svg"
+            alt="Water Bottle"
+            width="280px"
+            height="210px"
+          />
         </div>
       </section>
     </>
