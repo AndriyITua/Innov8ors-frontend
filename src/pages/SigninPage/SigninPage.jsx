718f0088fffaca6abd47f58ebbaed11d78baf557
andrewwwmatsko <matskoandrew@gmail.com>
2024-10-18 22:20:42 +0300

    mobile styles + form logic

--- src/pages/SigninPage/SigninPage.jsx
+++ src/pages/SigninPage/SigninPage.jsx
@@ -1,3 +1,18 @@
+import SignInAuthForm from "../../components/SignInAuthForm/SignInAuthForm.jsx";
+
+import Container from "../../components/Container/Container.jsx";
+
+import css from "./SigninPage.module.css";
+
 export default function SigninPage() {
-  return <p>SigninPage</p>;
+  return (
+    <main>
+      <section className={css.section}>
+        <Container>
+          <h2 className={css.title}>Sign In</h2>
+          <SignInAuthForm />
+        </Container>
+      </section>
+    </main>
+  );
 }
