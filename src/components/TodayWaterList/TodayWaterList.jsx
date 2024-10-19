828981620cc124f0481a12e37ab1059be685cc18
Volodymyr892 <sevcukvolodimir896@gmail.com>
2024-10-19 17:18:06 +0300

    create today water list

---
+++ src/components/TodayWaterList/TodayWaterList.jsx
@@ -0,0 +1,131 @@
+import Container from "../Container/Container"
+import { HiOutlinePlus } from "react-icons/hi";
+import { HiOutlinePencilSquare } from "react-icons/hi2";
+import css from "./TodayWaterList.module.css"
+import  glassWater from "../../assets/homePageImages/glassWater.svg"
+import  deleteW from "../../assets/homePageImages/delete.svg"
+export default function TodayWaterList() {
+    return (
+        <Container>
+        <div className={css.container}>
+            <p className={css.title}>Today</p>
+            <div className={css.contItem}>
+            <ul className={css.item}>
+                    <li>
+                        <img className={css.img} src={glassWater} alt="glass water"/>
+                    </li>
+                    <div className={css.time}>
+                        <li className={css.water}>200 ml</li>
+                        <li className={css.am}>14:00 PM</li>
+                    </div>
+                    <div className={css.editDel}>
+                        <li >
+                            <button className={css.buttonIcon}><HiOutlinePencilSquare/></button>
+                        </li>
+                        <li>
+                           <button className={css.buttonDel}> <img src={deleteW} alt="Delete"/></button>
+                        </li>
+                    </div>
+                </ul>
+                <hr className={css.divider} />
+                <ul className={css.item}>
+                    <li>
+                        <img className={css.img} src={glassWater} alt="glass water"/>
+                    </li>
+                    <div className={css.time}>
+                        <li className={css.water}>200 ml</li>
+                        <li className={css.am}>14:00 PM</li>
+                    </div>
+                    <div className={css.editDel}>
+                        <li >
+                            <button className={css.buttonIcon}><HiOutlinePencilSquare/></button>
+                        </li>
+                        <li>
+                           <button className={css.buttonDel}> <img src={deleteW} alt="Delete"/></button>
+                        </li>
+                    </div>
+                </ul>
+                <hr className={css.divider} />
+                <ul className={css.item}>
+                    <li>
+                        <img className={css.img} src={glassWater} alt="glass water"/>
+                    </li>
+                    <div className={css.time}>
+                        <li className={css.water}>200 ml</li>
+                        <li className={css.am}>14:00 PM</li>
+                    </div>
+                    <div className={css.editDel}>
+                        <li >
+                            <button className={css.buttonIcon}><HiOutlinePencilSquare/></button>
+                        </li>
+                        <li>
+                           <button className={css.buttonDel}> <img src={deleteW} alt="Delete"/></button>
+                        </li>
+                    </div>
+                </ul>
+                <hr className={css.divider} />
+                <ul className={css.item}>
+                    <li>
+                        <img className={css.img} src={glassWater} alt="glass water"/>
+                    </li>
+                    <div className={css.time}>
+                        <li className={css.water}>200 ml</li>
+                        <li className={css.am}>14:00 PM</li>
+                    </div>
+                    <div className={css.editDel}>
+                        <li >
+                            <button className={css.buttonIcon}><HiOutlinePencilSquare/></button>
+                        </li>
+                        <li>
+                           <button className={css.buttonDel}> <img src={deleteW} alt="Delete"/></button>
+                        </li>
+                    </div>
+                </ul>
+                <hr className={css.divider} />
+                <ul className={css.item}>
+                    <li>
+                        <img className={css.img} src={glassWater} alt="glass water"/>
+                    </li>
+                    <div className={css.time}>
+                        <li className={css.water}>200 ml</li>
+                        <li className={css.am}>14:00 PM</li>
+                    </div>
+                    <div className={css.editDel}>
+                        <li >
+                            <button className={css.buttonIcon}><HiOutlinePencilSquare/></button>
+                        </li>
+                        <li>
+                           <button className={css.buttonDel}> <img src={deleteW} alt="Delete"/></button>
+                        </li>
+                    </div>
+                </ul>
+                <hr className={css.divider} />
+                <ul className={css.item}>
+                    <li>
+                        <img className={css.img} src={glassWater} alt="glass water"/>
+                    </li>
+                    <div className={css.time}>
+                        <li className={css.water}>200 ml</li>
+                        <li className={css.am}>14:00 PM</li>
+                    </div>
+                    <div className={css.editDel}>
+                        <li >
+                            <button className={css.buttonIcon}><HiOutlinePencilSquare/></button>
+                        </li>
+                        <li>
+                           <button className={css.buttonDel}> <img src={deleteW} alt="Delete"/></button>
+                        </li>
+                    </div>
+                </ul>
+                <hr className={css.divider} />
+                
+                <div className={css.buttonAddWaterCont}>
+                    <button type="submit" className={css.button}>
+                        <HiOutlinePlus className={css.icon}/>
+                        Add water</button>
+                </div>
+            </div>
+        </div>
+        </Container>
+    )
+}
