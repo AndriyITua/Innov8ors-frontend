2eed3a9e39bbfbbca840e567de88527d8af9168d
Sergii Stoiachko <119351759+SergiiSt@users.noreply.github.com>
2024-10-19 16:11:39 +0200

    bug fix + delete container WaterRatioPanel

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,17 @@
-import Container from "../Container/Container";
+import Container from "../../components/Container/Container";
 import WaterRatioPanel from "../../components/WaterRatioPanel/WaterRatioPanel";
 import css from "./HomePage.module.css";
 
 export default function HomePage() {
   return (
-    <section className={css.homePage}>
-      <Container>
-        <WaterRatioPanel />
-        <div className={css.verticalLine}></div>
-      </Container>
-    </section>
+    <main>
+      <section className={css.homePage}>
+        <Container>
+          <section className={css.WaterRatio}>
+            <WaterRatioPanel />
+          </section>
+        </Container>
+      </section>
+    </main>
   );
 }
