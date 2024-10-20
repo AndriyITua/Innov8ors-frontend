4c431f26f9302098fceb56bdefa9bf6b764f4df3
Volodymyr892 <sevcukvolodimir896@gmail.com>
2024-10-20 23:39:06 +0300

    fix

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,11 @@
 import css from "./HomePage.module.css";
 import WaterRatioPanel from "../../components/WaterRatioPanel/WaterRatioPanel";
-import  TodayWaterList  from "../../components/TodayWaterList/TodayWaterList";
 
 
 export default function HomePage() {
   return <div>
     <p>Home page</p>
     <WaterRatioPanel/>
-    <TodayWaterList/>
   </div> 
   
     
