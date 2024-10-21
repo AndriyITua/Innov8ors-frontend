50959b520d2529cca7374f30d15893d31b74747d
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2024-10-21 23:07:55 +0300

    logout стили
    планшет и десктоп

--- src/components/UserLogoutForm/UserLogoutForm.jsx
+++ src/components/UserLogoutForm/UserLogoutForm.jsx
@@ -7,16 +7,16 @@ const UserLogoutForm = ({ closeLogoutModal }) => {
       <div className={css.logoutTitle}>
         <h2 className={css.logoutTitleText}>Log Out</h2>
         <button className={css.closeButton} onClick={closeLogoutModal}>
-          <TfiClose className={css.closeIcon}/>
+          <TfiClose className={css.closeIcon} />
         </button>
       </div>
       <p className={css.logoutText}>Do you really want to leave</p>
       <div className={css.buttonList}>
-        <button className={css.cancelButton} onClick={closeLogoutModal}>
-          Cancel
-        </button>
         <button type="submit" className={css.logoutbutton}>
           Log Out
+        </button>
+        <button className={css.cancelButton} onClick={closeLogoutModal}>
+          Cancel
         </button>
       </div>
     </div>
