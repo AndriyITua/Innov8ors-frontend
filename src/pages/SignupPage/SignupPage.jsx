7cadd40cde607789562606354b137649eb8d23fe
kholshevnikova <151967304+kholshevnikova@users.noreply.github.com>
2024-10-28 15:31:14 +0200

    add loader

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,15 +1,24 @@
 import AuthForm from "../../components/AuthForm/AuthForm";
 // import Container from "../../components/Container/Container";
 import css from "./SignupPage.module.css";
+import { selectLoading } from "../../redux/auth/selectors";
+import Loader from "../../components/Loader/Loader";
+import { useSelector } from "react-redux";
 
 export default function SignupPage() {
+  const isLoading = useSelector(selectLoading);
+
   return (
     <>
       <section className={css.signupSection}>
-        <div className={css.container}>
-          <h3 className={css.formText}>Sign Up</h3>
-          <AuthForm />
-        </div>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <div className={css.container}>
+            <h3 className={css.formText}>Sign Up</h3>
+            <AuthForm />
+          </div>
+        )}
       </section>
     </>
   );
