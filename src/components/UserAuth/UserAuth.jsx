a363a619fe45c504acb89cb5b55ff5bf19d00a14
MikhayloBil <152810994+MikhayloBil@users.noreply.github.com>
2024-10-31 14:07:41 +0200

    header-hover

--- src/components/UserAuth/UserAuth.jsx
+++ src/components/UserAuth/UserAuth.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import css from "./UserAuth.module.css";
 import Avatar from "../../assets/icons/Avatar";
 
 const UserAuth = () => {
+  const [isHovered, setIsHovered] = useState(false);
   const navigate = useNavigate();
+
   return (
-    <button className={css.button} onClick={() => navigate("/signin")}>
-      <span className={css.span}>Sign in</span>
-      <Avatar />
+    <button
+      className={css.button}
+      onClick={() => navigate("/signin")}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <span className={isHovered ? css.spanHovered : css.span}>Sign in</span>
+      <Avatar
+        color={isHovered ? "var(--secondary-color-orange, #ff9d43)" : "#2F2F2F"}
+      />
     </button>
   );
 };
